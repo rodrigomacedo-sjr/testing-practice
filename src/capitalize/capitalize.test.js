123147eadcf2ee87580df6fab6daa3f701b0ac3e
Rodrigo Macedo Silva Junior <rodrigo.junior@platformscience.com>
2026-05-21 14:37:10 -0300

    feat: improve tests
    Signed-off-by: Rodrigo Macedo Silva Junior <rodrigo.junior@platformscience.com>

--- src/capitalize/capitalize.test.js
+++ src/capitalize/capitalize.test.js
@@ -1,7 +1,13 @@
 import capitalize from "./capitalize.js";
 
-it("A single letter is capitalized", () => {
+it("A single lowercase letter is capitalized", () => {
   expect(capitalize("a")).toBe("A");
   expect(capitalize("b")).toBe("B");
   expect(capitalize("c")).toBe("C");
 });
+
+it("A single uppercase letter is unchanged", () => {
+  expect(capitalize("A")).toBe("A");
+  expect(capitalize("B")).toBe("B");
+  expect(capitalize("C")).toBe("C");
+})
