d59417f28b499b064efed2b23d79fdde235b50d2
Rodrigo Macedo Silva Junior <rodrigo.junior@platformscience.com>
2026-05-23 17:34:46 -0300

    feat: improve tests
    Signed-off-by: Rodrigo Macedo Silva Junior <rodrigo.junior@platformscience.com>

--- src/reverse/reverse.test.js
+++ src/reverse/reverse.test.js
@@ -2,4 +2,6 @@ import reverse from "./reverse.js";
 
 it("Reverses single word", () => {
   expect(reverse("banana")).toBe("ananab");
+  expect(reverse("apple")).toBe("elppa");
+  expect(reverse("strawberry")).toBe("yrrebwarts");
 })
