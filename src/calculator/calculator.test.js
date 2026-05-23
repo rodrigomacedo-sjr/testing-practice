3beefea73ad78a8cb9005922f9d91d653880c23a
Rodrigo Macedo Silva Junior <rodrigo.junior@platformscience.com>
2026-05-23 17:54:32 -0300

    feat: add failing tests
    Signed-off-by: Rodrigo Macedo Silva Junior <rodrigo.junior@platformscience.com>

--- src/calculator/calculator.test.js
+++ src/calculator/calculator.test.js
@@ -9,3 +9,11 @@ it("Calculator has the expected shape", () => {
   expect(typeof calculator.divide === "function").toBe(true);
   expect(typeof calculator.multiply === "function").toBe(true);
 });
+
+it("Subtract works for numbers", () => {
+  expect(calculator.subtract(1, 2)).toBe(-1);
+  expect(calculator.subtract(2, 1)).toBe(1);
+  expect(calculator.subtract(10000000000, 1)).toBe(9999999999);
+  expect(calculator.subtract(1, 10000000000)).toBe(-9999999999);
+  expect(calculator.subtract(1.23, 4.56)).toBe(-3.33);
+})
