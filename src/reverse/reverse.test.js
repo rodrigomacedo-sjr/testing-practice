170901530bea1168baa6c5c33f41399b02878f1e
Rodrigo Macedo Silva Junior <rodrigo.junior@platformscience.com>
2026-05-23 17:40:58 -0300

    feat: add failing and passing new tests
    Signed-off-by: Rodrigo Macedo Silva Junior <rodrigo.junior@platformscience.com>

--- src/reverse/reverse.test.js
+++ src/reverse/reverse.test.js
@@ -5,3 +5,19 @@ it("Reverses single word", () => {
   expect(reverse("apple")).toBe("elppa");
   expect(reverse("strawberry")).toBe("yrrebwarts");
 })
+
+it("Reverses phrase", () => {
+  expect(reverse("I am just a guy.")).toBe(".yug a tsuj ma I");
+  expect(reverse("Painting more than 100 Bob Goods!")).toBe("!sdooG boB 001 naht erom gnitniaP");
+})
+
+it("Deals with special characters", () => {
+  expect(reverse("\n100%&#$")).toBe("$#&%001\n");
+})
+
+it("Deals with empty strings", () => {
+  expect(reverse().toBe(""));
+  expect(reverse("").toBe(""));
+  expect(reverse("   ").toBe("   "));
+  expect(reverse("      ").toBe("     "));
+})
