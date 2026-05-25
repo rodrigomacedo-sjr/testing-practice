3ac68bce9dad6e25469582f93a1d183970cb14a6
Rodrigo Macedo Silva Junior <rodrigo.junior@platformscience.com>
2026-05-25 13:59:45 -0300

    feat: add failing tests
    Signed-off-by: Rodrigo Macedo Silva Junior <rodrigo.junior@platformscience.com>

---
+++ src/analyzeArray/analyzeArray.test.js
@@ -0,0 +1,51 @@
+import analyzeArray from "./analyzeArray.js";
+
+it("Handles simple integer arrays", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+
+  expect(analyzeArray([5, 5, 5, 5, 5])).toEqual({
+    average: 5,
+    min: 5,
+    max: 5,
+    length: 5,
+  });
+
+  expect(analyzeArray([5, -5, -5, -5, 5])).toEqual({
+    average: -1,
+    min: -5,
+    max: 5,
+    length: 5,
+  });
+});
+
+it("Handles invalid values", () => {
+  expect(analyzeArray(1)).toEqual({
+    error: "Not an array",
+  });
+
+  expect(analyzeArray("thing")).toEqual({
+    error: "Not an array",
+  });
+
+  expect(analyzeArray()).toEqual({
+    error: "Not an array",
+  });
+
+  expect(analyzeArray([1, 2, "3", 4, 5])).toEqual({
+    error: "contains NaN",
+  });
+});
+
+it("Handles empty array", () => {
+  expect(analyzeArray([])).toEqual({
+    average: 0,
+    min: "NA",
+    max: "NA",
+    length: 0,
+  });
+});
